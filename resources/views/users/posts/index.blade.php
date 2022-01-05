c55bb6083f5e55185922e72f33bb4994987229bc
Ryan McBride <ryanmcbride@Ryans-MacBook-Pro.local>
2022-01-05 10:38:35 -0500

    Laravel Project

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,7 +2,15 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            {{$user->name}}
+            @if($posts->count())
+                @foreach($posts as $post)
+                    <x-post :post="$post"></x-post>
+                @endforeach
+                {{ $posts->links() }}
+            @else
+                <p>{{$user->name}} does not have any posts</p>
+            @endif
+
         </div>
     </div>
 
